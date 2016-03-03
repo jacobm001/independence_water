48d52dde9112932ca5ddec83abc44d30545f8d39
Alexander Merrill <merrilal@onid.oregonstate.edu>
2016-03-03 14:14:03 -0800

    Updated chart example

--- gchart/combo.js
+++ gchart/combo.js
@@ -5,20 +5,28 @@ google.charts.setOnLoadCallback(drawVisualization);
 function drawVisualization() {
 	// Some raw data (not necessarily accurate)
 	var data = google.visualization.arrayToDataTable([
-		['Month', 'Bolivia', 'Ecuador', 'Madagascar', 'Papua New Guinea', 'Rwanda', 'Average'],
-		['2004/05',  165,      938,         522,             998,           450,      614.6],
-		['2005/06',  135,      1120,        599,             1268,          288,      682],
-		['2006/07',  157,      1167,        587,             807,           397,      623],
-		['2007/08',  139,      1110,        615,             968,           215,      609.4],
-		['2008/09',  136,      691,         629,             1026,          366,      569.6]
+		['Date',	'Daily Use',	'Running Total',	'Average'],
+		['3/1/2016',	8,	8,	8],
+		['3/2/2016',	10,	18,	9],
+		['/3/2016',	7,	25,	8.333333333],
+		['/4/2016',	8,	3,	8.25],
+		['/5/2016',	7,	40,	8],
+		['/6/2016',	15,	55,	9.166666667],
+		['/7/2016',	13,	68,	9.714285714],
+		['/8/2016',	12,	80,	10],
+		['/9/2016',	14,	94,	10.44444444],
+		['/10/2016',	11,	105,	10.5],
+		['/11/2016',	0,	105,	9.545454545],
+		['/12/2016',	7,	112,	9.333333333],
+		['/13/2016',	8,	120,	9.230769231]
 	]);
 
 	var options = {
-		title : 'Monthly Coffee Production by Country',
-		vAxis: {title: 'Cups'},
-		hAxis: {title: 'Month'},
+		title : 'Daily Water Use',
+		vAxis: {title: 'Water Use'},
+		hAxis: {title: 'Date'},
 		seriesType: 'bars',
-		series: {5: {type: 'line'}}
+		series: {2: {type: 'line'}}
 	};
 
 	var chart = new google.visualization.ComboChart(document.getElementById('chart_div'));
