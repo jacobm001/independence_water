f59e449b69048257c2ef20e70bc97684a7db57dd
Jacob Mastel <jacob@mastel.org>
2016-03-27 20:45:52 -0700

    added a unique column combination requirement

--- storage/schema.sql
+++ storage/schema.sql
@@ -2,7 +2,8 @@ create table if not exists meter_read (
 	read_id integer primary key autoincrement,
 	meter_id integer not null,
 	value integer not null,
-	time_read date not null
+	time_read date not null,
+	constraint meter_read_unq unique (meter_id, value, time_read)
 );
 
 create index if not exists meter_read_sorted on meter_read(meter_id,time_read);
