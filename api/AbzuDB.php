e7a4c2382056fdae9a4d82115e602204317376fd
jacob <jacob@mastel.org>
2016-04-05 16:51:21 -0700

    started on Tyler's interface

--- api/AbzuDB.php
+++ api/AbzuDB.php
@@ -2,12 +2,33 @@
 	class AbzuDB
 	{
 		private $db;
+		private $valid_intervals = [
+			"day",
+			"month",
+			"week",
+			"year"
+		];
 		
 		public function __constructor(&$db)
 		{
 			$this->db = $db;
 		}
+
+		private function validate_interval($interval)
+		{
+			if($interval in $this->valid_intervals)
+				return true;
+			else
+				return false;
+		}
 		
-		
+		public function get_meter_data($meter, $interval) 
+		{
+			if($this->validate_interval($interval) == false)
+				throw new Exception("Bad interval value.");
+			
+			$query = "select strftime(:tformat, time_read) as `timestamp`, sum(value) from meter_read where meter_id = :meter_id group by strftime(:tformat, time_read)";
+			$stmt = $this->db->prepare($query);
+		}
 	}
 ?>
