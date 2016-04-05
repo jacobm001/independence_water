e4cb7da37d9b590dd29543c5e3bb328a7c7bf771
Jacob Mastel <jacob@mastel.org>
2016-04-04 19:17:21 -0700

    fixed directory variable problem

--- api/Init.php
+++ api/Init.php
@@ -1,14 +1,14 @@
 <?php 
 	// Configuration area
 	$GLOBALS['storage_directory']  = '../storage';
-	$GLOBALS['database_direcotry'] = $GLOBALS['storage_directory'] . '/data.db';
+	$GLOBALS['database_path'] = $GLOBALS['storage_directory'] . '/data.db';
 
 	// Ensure datbase exists
 	// See 
-	if(file_exists($GLOBALS['database_directory'])) {
-		$db = new PDO($GLOBALS['database_directory']);
+	if(file_exists($GLOBALS['database_path'])) {
+		$db = new PDO("sqlite:" . $GLOBALS['database_path']);
 	} else {
-		$db            = new PDO($GLOBALS['database_directory']);
+		$db            = new PDO("sqlite:" . $GLOBALS['database_path']);
 		$db_create_sql = file_get_contents($GLOBALS['storage_directory'] . '/schema.sql');
 		$db->exec($db_create_sql);
 	}
