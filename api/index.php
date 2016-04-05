bd8849ad17ed81499bf58ab45320a7cf243c3c10
Tyler Fosback <fosbackt@oregonstate.edu>
2016-04-04 20:27:00 -0700

    Move route class to route.php

--- api/index.php
+++ api/index.php
@@ -1,40 +1,14 @@
 <?php 
 	include 'Init.php';
 	include 'DataImporter.php';
-    
-    class Route{
-        private $_uri = [];
-        private $_method = [];
-        
-        public function add($uri, $method = null)
-        {
-            $this->_uri[] = trim($uri, '/');
-            
-            if ($method != null)
-            {
-                $this->_method[] = $method;
-            }
-        }
-    
-        public function submit()
-        {
-            echo $uriGetParam = isset($_GET['uri']) ? '/' . $_GET['uri'] : '/';
-            
-            foreach ($this->_uri as $key => $value)
-            {
-                if (preg_match("#$^value$#", $uriGetParam))
-                {
-                    $useMethod = $this->_method[$key];
-                    new $useMethod;
-                }
-            }
-        }
-    }
-    
-    $route = new Route;
-    
-    route->add('day');
-    route->add('week');
-    route->add('month');
-    route->add('year');
+	include 'Route.php';
+	
+	$route = new Route();
+	
+	route->add('read/day');
+	route->add('read/week');
+	route->add('read/month');
+	route->add('read/year');
+	
+	route->submit();
 ?>
