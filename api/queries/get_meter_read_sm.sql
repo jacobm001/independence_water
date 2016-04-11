880501dc00677b71596a4c7abc4a2770e6d8a26d
Jacob Mastel <jacob@mastel.org>
2016-04-10 17:42:50 -0700

    split to two seperate read queries

--- api/queries/get_meter_read_sm.sql
+++ api/queries/get_meter_read_sm.sql
@@ -1,4 +1,4 @@
-select 
+select distinct
 	strftime(:tformat, time_read) as `timestamp`,
 	(
 		select
@@ -8,14 +8,14 @@ select
 		where
 			mr.meter_id = meter_read.meter_id
 			and strftime(:tformat, mr.time_read) = strftime(:tformat, meter_read.time_read)
-	) - (
+	) - ( 
 		select
-			min(mr.value)
+			max(mr.value)
 		from
 			meter_read mr
 		where
 			mr.meter_id = meter_read.meter_id
-			and strftime(:tformat, mr.time_read) = strftime(:tformat, meter_read.time_read)
+			and strftime(:tformat, mr.time_read) = strftime(:tformat, meter_read.time_read, :dec_value)
 	) as value
 from
 	meter_read
