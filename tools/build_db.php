cd08ec800e62c7f8ec34771b827a5119d65f951b
Jacob Mastel <jacob@mastel.org>
2016-04-10 20:15:26 -0700

    build_db will now add addresses

--- tools/build_db.php
+++ tools/build_db.php
@@ -6,8 +6,20 @@
 	$objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator("./historical_data"));
 	$Regex   = new RegexIterator($objects, '/^.+\.txt$/i', RecursiveRegexIterator::GET_MATCH);
 	
+	$query = "insert into meter_info(`meter_id`, `address`) values(:meter_id, :address);";
+	$stmt = $db->prepare($query);
+
 	foreach($Regex as $name => $object) {
 		echo "Importing file: " . $name . "\n";
-		new DataImporter($db, file_get_contents($name));
+		$DI = new DataImporter($db, file_get_contents($name));
+		
+		$str = explode("/", $name);
+		if(count($str) == 4) {
+			$id = $DI->get_meter_id();
+			
+			$stmt->bindParam(':meter_id', $id);
+			$stmt->bindParam(':address', $str[2]);
+			$stmt->execute();
+		}
 	}
 ?>
