c3c069e030e2e7f03b12be86c58be3c33078ab08
Jacob Mastel <jacob@mastel.org>
2016-04-10 20:23:07 -0700

    handle the datafile for the unkown address

--- tools/build_db.php
+++ tools/build_db.php
@@ -6,19 +6,32 @@
 	$objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator("./historical_data"));
 	$Regex   = new RegexIterator($objects, '/^.+\.txt$/i', RecursiveRegexIterator::GET_MATCH);
 	
-	$query = "insert into meter_info(`meter_id`, `address`) values(:meter_id, :address);";
-	$stmt = $db->prepare($query);
+	
 
 	foreach($Regex as $name => $object) {
 		echo "Importing file: " . $name . "\n";
 		$DI = new DataImporter($db, file_get_contents($name));
-		
+
+		// most of the supplied files have the address as part of the filename		
 		$str = explode("/", $name);
 		if(count($str) == 4) {
+			$query = "insert into meter_info(`meter_id`, `address`) values(:meter_id, :address);";
+			$stmt = $db->prepare($query);
+
 			$id = $DI->get_meter_id();
 			
 			$stmt->bindParam(':meter_id', $id);
 			$stmt->bindParam(':address', $str[2]);
+			$stmt->execute();
+		}
+		// insert what doesn't fit the pattern described above
+		else {
+			$query = "insert into meter_info(`meter_id`, `address`) values(:meter_id, 'unknown');";
+			$stmt = $db->prepare($query);
+
+			$id = $DI->get_meter_id();
+			
+			$stmt->bindParam(':meter_id', $id);
 			$stmt->execute();
 		}
 	}
