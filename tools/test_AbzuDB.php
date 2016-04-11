788f2e0bc7627d34742161dbaa102beb75b5b5ee
Jacob Mastel <jacob@mastel.org>
2016-04-10 19:33:54 -0700

    playing with more tests

--- tools/test_AbzuDB.php
+++ tools/test_AbzuDB.php
@@ -4,5 +4,6 @@
 	require "../api/AbzuDB.php";
 
 	$AbzuDB = new AbzuDB($db);
-	$AbzuDB->get_meter_data("7813517", "week");
+	// $AbzuDB->get_meter_data("7813517", "week");
+	// $AbzuDB->insert_meter_data("042014", "01245", "10/Oct/2000:13:55:36 -0700");
 ?>
