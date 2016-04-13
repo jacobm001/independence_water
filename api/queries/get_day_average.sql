89b3dac1394b4d2322930f6a4aec03ea58f865f7
Jacob Mastel <jacob@mastel.org>
2016-04-12 19:49:49 -0700

    fixed day avergae query

--- api/queries/get_day_average.sql
+++ api/queries/get_day_average.sql
@@ -1,28 +1,28 @@
-with cte as (
-	select
-		(
-			select
-				max(mr.value)
-			from
-				meter_read mr
-			where
-				mr.meter_id = meter_read.meter_id
-				and strftime("%Y-%m-%d", mr.time_read) = strftime("%Y-%m-%d", meter_read.time_read)
-		) - ( 
-			select
-				max(mr.value)
-			from
-				meter_read mr
-			where
-				mr.meter_id = meter_read.meter_id
-				and strftime("%Y-%m-%d", mr.time_read) = strftime("%Y-%m-%d", meter_read.time_read, "-1 day")
-		) as value
-	from
-		meter_read
-	where
-		meter_id = :meter_id
-)
 select
-	avg(value) as value
+	avg(value)
 from
-	cte;
+	(
+		select
+			meter_id,
+			(
+				select
+					max(mr.value)
+				from
+					meter_read mr
+				where
+					mr.meter_id = meter_read.meter_id
+					and strftime("%Y-%m-%d", mr.time_read) = strftime("%Y-%m-%d", meter_read.time_read)
+			) - ( 
+				select
+					max(mr.value)
+				from
+					meter_read mr
+				where
+					mr.meter_id = meter_read.meter_id
+					and strftime("%Y-%m-%d", mr.time_read) = strftime("%Y-%m-%d", meter_read.time_read, "-1 day")
+			) as value
+			from
+				meter_read
+	)
+where
+	meter_id = :meter_id
