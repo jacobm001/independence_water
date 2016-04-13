8d1e3008c02194a5c16bf2883b4a695add1f7a03
Tyler Fosback <fosbackt@oregonstate.edu>
2016-04-12 19:00:46 -0700

    Change the route indexing by one.

--- api/Route.php
+++ api/Route.php
@@ -24,50 +24,50 @@
 			$params = explode("/", $_GET['uri']);
 			
 			//if ($params[0] == "api"){
-				if ($params[2] == "daychart"){
-					echo $this->AbzuDB->get_meter_data($params[1], "day");
+				if ($params[1] == "daychart"){
+					echo $this->AbzuDB->get_meter_data($params[0], "day");
 				}
-				if ($params[2] == "weekchart"){
-					echo $this->AbzuDB->get_meter_data($params[1], "week");
+				if ($params[1] == "weekchart"){
+					echo $this->AbzuDB->get_meter_data($params[0], "week");
 				}
-				if ($params[2] == "monthchart"){
-					echo $this->AbzuDB->get_meter_data($params[1], "month");
+				if ($params[1] == "monthchart"){
+					echo $this->AbzuDB->get_meter_data($params[0], "month");
 				}
-				if ($params[2] == "daytotalchart"){
-					echo $this->AbzuDB->get_meter_data($params[1], "day");
+				if ($params[1] == "daytotalchart"){
+					echo $this->AbzuDB->get_meter_data($params[0], "day");
 				}
-				if ($params[2] == "weektotalchart"){
-					echo $this->AbzuDB->get_meter_data($params[1], "week");
+				if ($params[1] == "weektotalchart"){
+					echo $this->AbzuDB->get_meter_data($params[0], "week");
 				}
-				if ($params[2] == "monthtotalchart"){
-					echo $this->AbzuDB->get_meter_data($params[1], "month");
+				if ($params[1] == "monthtotalchart"){
+					echo $this->AbzuDB->get_meter_data($params[0], "month");
 				}
-				if ($params[2] == "daytable"){
-					echo $this->AbzuDB->get_meter_data($params[1], "day");
+				if ($params[1] == "daytable"){
+					echo $this->AbzuDB->get_meter_data($params[0], "day");
 				}
-				if ($params[2] == "weektable"){
-					echo $this->AbzuDB->get_meter_data($params[1], "week");
+				if ($params[1] == "weektable"){
+					echo $this->AbzuDB->get_meter_data($params[0], "week");
 				}
-				if ($params[2] == "monthtable"){
-					echo $this->AbzuDB->get_meter_data($params[1], "month");
+				if ($params[1] == "monthtable"){
+					echo $this->AbzuDB->get_meter_data($params[0], "month");
 				}
-				if ($params[2] == "dayaverage"){
-					echo $this->AbzuDB->get_gauge_to_date($params[1], "day", $params[3]);
+				if ($params[1] == "dayaverage"){
+					echo $this->AbzuDB->get_gauge_to_date($params[0], "day", $params[3]);
 				}
-				if ($params[2] == "monthtodate"){
-					echo $this->AbzuDB->get_gauge_to_date($params[1], "month", $params[3]);
+				if ($params[1] == "monthtodate"){
+					echo $this->AbzuDB->get_gauge_to_date($params[0], "month", $params[3]);
 				}
-				if ($params[2] == "yeartodate"){
-					echo $this->AbzuDB->get_gauge_to_date($params[1], "year", $params[3]);
+				if ($params[1] == "yeartodate"){
+					echo $this->AbzuDB->get_gauge_to_date($params[0], "year", $params[3]);
 				}
-				if ($params[2] == "daycityaverage"){
-					echo $this->AbzuDB->get_city_avg_data($params[1], "day");
+				if ($params[1] == "daycityaverage"){
+					echo $this->AbzuDB->get_city_avg_data($params[0], "day");
 				}
-				if ($params[2] == "weekcityaverage"){
-					echo $this->AbzuDB->get_city_avg_data($params[1], "week");
+				if ($params[1] == "weekcityaverage"){
+					echo $this->AbzuDB->get_city_avg_data($params[0], "week");
 				}
-				if ($params[2] == "monthcityaverage"){
-					echo $this->AbzuDB->get_city_avg_data($params[1], "month");
+				if ($params[1] == "monthcityaverage"){
+					echo $this->AbzuDB->get_city_avg_data($params[0], "month");
 				}
 			// } else {
 			// 	echo "Invalid route";
