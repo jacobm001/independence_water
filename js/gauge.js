3eca2175141023f6043033446c8e0231451036ef
Alexander Merrill <merrilal@onid.oregonstate.edu>
2016-04-12 17:13:42 -0700

    using routes for charts and gauges Corrected typo in index

--- js/gauge.js
+++ js/gauge.js
@@ -8,12 +8,27 @@ config1.textVertPosition = 0.8;
 config1.waveAnimateTime = 1000;
 config1.waveHeight = 0.05;
 config1.waveAnimate = true;
-config1.waveRise = false;
-config1.waveHeightScaling = false;
+//config1.waveRise = false;
+//config1.waveHeightScaling = false;
 config1.waveOffset = 0.25;
 config1.waveCount = 3;
 config1.displayPercent = false;
-var gauge1 = loadLiquidFillGauge("fillgauge1", 55, config1);
+/*
+config1.minValue = 0;
+config1.maxValue = 5000;
+*/
+$.get("../api/" + meterID + "/yeartodate", function(csvString) {
+	// transform the CSV string into an n-dimensional array, where n is the number of columns in the csv
+	var arrayData = $.csv.toArrays(csvString, {onParseValue: $.csv.hooks.castToScalar});
+/*	if( arrayData.length > 1) {
+		error check here
+	} else {
+*/
+	config1.maxValue = arrayData[0] + 500;
+	var gauge1 = loadLiquidFillGauge("fillgauge1", arrayData[0], config1);
+	}
+}
+//var gauge1 = loadLiquidFillGauge("fillgauge1", 4580, config1);
 
 var config2 = liquidFillGaugeDefaultSettings();
 config2.circleColor = "#00CCFF";
@@ -26,7 +41,22 @@ config2.textVertPosition = 0.8;
 config2.waveHeight = 0.3;
 config2.waveCount = 1;
 config2.displayPercent = false;
-var gauge2= loadLiquidFillGauge("fillgauge2", 28, config2);
+/*
+config2.minValue = 0;
+config2.maxValue = 500;
+*/
+$.get("../api/" + meterID + "/monthtodate", function(csvString) {
+	// transform the CSV string into an n-dimensional array, where n is the number of columns in the csv
+	var arrayData = $.csv.toArrays(csvString, {onParseValue: $.csv.hooks.castToScalar});
+/*	if( arrayData.length > 1) {
+		error check here
+	} else {
+*/
+	config2.maxValue = arrayData[0] + 50;
+	var gauge2= loadLiquidFillGauge("fillgauge2", arrayData[0], config2);
+	}
+}
+//var gauge2= loadLiquidFillGauge("fillgauge2", 397, config2);
 
 var config3 = liquidFillGaugeDefaultSettings();
 config3.circleThickness = 0.075;
@@ -37,7 +67,21 @@ config3.textVertPosition = 0.8;
 config3.waveOffset = 0.25;
 config3.valueCountUp = false;
 config3.displayPercent = false;
-var gauge3 = loadLiquidFillGauge("fillgauge3", 60.1, config3);
+/*
+config3.minValue = 0;
+config3.maxValue = 30;
+$.get("../api/" + meterID + "/dayaverage", function(csvString) {
+	// transform the CSV string into an n-dimensional array, where n is the number of columns in the csv
+	var arrayData = $.csv.toArrays(csvString, {onParseValue: $.csv.hooks.castToScalar});
+/*	if( arrayData.length > 1) {
+		error check here
+	} else {
+*/
+	config3.maxValue = arrayData[0] + 5;
+	var gauge3 = loadLiquidFillGauge("fillgauge3", arrayData[0], config3);
+	}
+}
+//var gauge3 = loadLiquidFillGauge("fillgauge3", 12.41666667, config3);
 
 function NewValue(){
 	if(Math.random() > .5){
