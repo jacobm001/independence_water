2980ddd89986cbc5a07c054d0d3683961cd2f8cb
jacob <jacob@mastel.org>
2016-04-28 17:55:26 -0700

    possible fix of to_date()

--- api/AbzuDB.php
+++ api/AbzuDB.php
@@ -28,8 +28,8 @@
 			$this->qry_meter_insert     = file_get_contents("../api/queries/insert_meter_data.sql");
 			$this->qry_city_avg_sm      = file_get_contents("../api/queries/get_city_avg_sm.sql");
 			$this->qry_city_avg_lg      = file_get_contents("../api/queries/get_city_avg_lg.sql");
-			$this->qry_guage_to_date_sm = file_get_contents("../api/queries/qry_guage_to_date_sm.sql");
-			$this->qry_guage_to_date_lg = file_get_contents("../api/queries/qry_guage_to_date_lg.sql");
+			$this->qry_guage_to_date_sm = file_get_contents("../api/queries/qet_guage_to_date_sm.sql");
+			$this->qry_guage_to_date_lg = file_get_contents("../api/queries/qet_guage_to_date_lg.sql");
 			$this->qry_get_day_avg      = file_get_contents("../api/queries/get_day_average.sql");
 		}
 
