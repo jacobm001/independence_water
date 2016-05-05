698f0976e6317897d0f41986c8e91af769af14e4
Alexander Merrill <merrilal@onid.oregonstate.edu>
2016-05-05 13:41:13 -0700

    added more collumsn to first row

--- js/allcharts.js
+++ js/allcharts.js
@@ -675,8 +675,8 @@ function drawDayTable() {
 		console.log(arrayData);
 
 		if (arrayData.length > 2){
-			var extra =["1"];
-			for (var i = 1; i < arrayData.length; i++){
+			var extra =[""];
+			for (var i = 0; i < arrayData.length; i++){
 				Array.prototype.push.apply(parseInt(arrayData[i]), extra);
 			}
 			console.log(arrayData);
