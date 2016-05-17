a32e28cf2c35a61f5a6822f7c85d39e8018ed484
Alexander Merrill <merrilal@onid.oregonstate.edu>
2016-05-17 14:50:24 -0700

    Username and password one group

--- login.php
+++ login.php
@@ -35,8 +35,6 @@ session_start();//session starts here
 						<fieldset>
 							<div class="form-group"  >
 								<input class="form-control" placeholder="Username" name="username" type="text" autofocus>
-							</div>
-							<div class="form-group">
 								<input class="form-control" placeholder="Password" name="pass" type="password" value="">
 							</div>
 
