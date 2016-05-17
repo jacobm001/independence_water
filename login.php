3dd50f49cc3935e541417257854001eec1d2bac7
Alexander Merrill <merrilal@onid.oregonstate.edu>
2016-05-17 14:58:38 -0700

    user icon in field

--- login.php
+++ login.php
@@ -34,7 +34,7 @@ session_start();//session starts here
 					<form role="form" method="post" action="login.php">
 						<fieldset>
 							<div class="form-group has-feedback">
-								<span class="glyphicon glyphicon-user form-control-feedback"></span>
+								<i class="glyphicon glyphicon-user form-control-feedback"></i>
 								<input class="form-control" placeholder="Username" name="username" type="text" autofocus>
 							</div>
 							<div class="form-group">
