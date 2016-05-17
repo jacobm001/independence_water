89e01f5e293e886808b1da2f4135cc209eeea0d1
Alexander Merrill <merrilal@onid.oregonstate.edu>
2016-05-17 14:51:02 -0700

    username and password back to 2 groups

--- login.php
+++ login.php
@@ -35,6 +35,8 @@ session_start();//session starts here
 						<fieldset>
 							<div class="form-group"  >
 								<input class="form-control" placeholder="Username" name="username" type="text" autofocus>
+							</div>
+							<div class="form-group">
 								<input class="form-control" placeholder="Password" name="pass" type="password" value="">
 							</div>
 
